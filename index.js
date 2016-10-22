e910f8985029e5e03a52350d7e046fecf7b2d029
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-10-22 23:09:15 +0200

    fix: uri encode only the title for hn news

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ var fs = require("fs");
 var Promise = require("bluebird");
 var Logdown = require("logdown");
 var logger = new Logdown({
-  prefix: "wordspread"
+    prefix: "wordspread"
 });
 var path = require("path");
 
 var form = require("form-urlencoded");
+var uri = require("strict-uri-encode");
 
 var _require2 = require("./lib/print");
 
@@ -21,15 +22,18 @@ var link = _require2.link;
 var msg = _require2.msg;
 
 var getOptions = function (doc) {
-  "use strict";
-  var o = docopt(doc);
-  var subreddit = o["--subreddit"] || "javascript";
-  var title = o.TITLE;
-  var url = o.LINK;
-  var noshow = o["--no-show-hn"] || false;
-  return {
-    subreddit: subreddit, title: title, url: url, noshow: noshow
-  };
+    "use strict";
+    var o = docopt(doc);
+    var subreddit = o["--subreddit"] || "javascript";
+    var title = o.TITLE;
+    var url = o.LINK;
+    var noshow = o["--no-show-hn"] || false;
+    return {
+        subreddit: subreddit,
+        title: title,
+        url: url,
+        noshow: noshow
+    };
 };
 
 var doc = fs.readFileSync(__dirname + "/docs/usage.md", "utf8");
@@ -37,66 +41,66 @@ var doc = fs.readFileSync(__dirname + "/docs/usage.md", "utf8");
 // https: //news.ycombinator.com/submitlink?u=https%3A%2F%2Fgithub.com%2Fvzaccaria%2Fdom-restyle&t=vzaccaria%2Fdom-restyle
 
 function postReddit(title, url, opts) {
-  var submiturl = "http://www.reddit.com/submit";
+    var submiturl = "http://www.reddit.com/submit";
 
-  var data = {
-    url: url,
-    title: title,
-    sr: opts.subreddit
-  };
-  // body...
-  return "" + submiturl + "?" + form.encode(data);
+    var data = {
+        url: url,
+        title: title,
+        sr: opts.subreddit
+    };
+    // body...
+    return "" + submiturl + "?" + form.encode(data);
 }
 
 function postHn(title, url, opts) {
-  "use strict";
-  var htitle;
-  if (!opts.noshow) {
-    htitle = "Show HN: " + title;
-  } else {
-    htitle = title;
-  }
-  url = url;
+    "use strict";
+    var htitle;
+    if (!opts.noshow) {
+        htitle = "Show HN: " + title;
+    } else {
+        htitle = title;
+    }
+    url = url;
 
-  var data = {
-    u: url,
-    t: htitle
-  };
-  return "https://news.ycombinator.com/submitlink?" + form.encode(data);
+    var data = {
+        u: url,
+        t: htitle
+    };
+    return "https://news.ycombinator.com/submitlink?u=" + uri(url) + "&t=" + uri(htitle);
 }
 
 function postEcho(title, url) {
-  "use strict";
+    "use strict";
 
-  var data = {
-    u: url,
-    t: title
-  };
-  return "http://www.echojs.com/submit?" + form.encode(data);
+    var data = {
+        u: url,
+        t: title
+    };
+    return "http://www.echojs.com/submit?" + form.encode(data);
 }
 
 function postTwitter(title, url) {
-  "use strict";
+    "use strict";
 
-  var data = {
-    url: url,
-    text: "" + title + " — "
-  };
-  return "http://twitter.com/share?" + form.encode(data);
+    var data = {
+        url: url,
+        text: "" + title + " — "
+    };
+    return "http://twitter.com/share?" + form.encode(data);
 }
 
 // javascript:(function(){f='http://twitter.com/share?url='+encodeURIComponent(window.location.href)+'&text='+encodeURIComponent(document.title);a=function(){if(!window.open(f))location.href=f};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()
 var main = function () {
-  "use strict";
-  var opts = getOptions(doc);
-  var title = opts.title;
-  var url = opts.url;
+    "use strict";
+    var opts = getOptions(doc);
+    var title = opts.title;
+    var url = opts.url;
 
-  msg();
-  logger.log("**HN**: ", link(postHn(title, url, opts)));
-  logger.log("**Reddit /r/" + opts.subreddit + "**: ", link(postReddit(title, url, opts)));
-  logger.log("**Echo JS**: ", link(postEcho(title, url)));
-  logger.log("**Twitter**: ", link(postTwitter(title, url)));
+    msg();
+    logger.log("**HN**: ", link(postHn(title, url, opts)));
+    logger.log("**Reddit /r/" + opts.subreddit + "**: ", link(postReddit(title, url, opts)));
+    logger.log("**Echo JS**: ", link(postEcho(title, url)));
+    logger.log("**Twitter**: ", link(postTwitter(title, url)));
 };
 
 main();
--- src/index.js
+++ src/index.js
@@ -1,99 +1,105 @@
 var {
-  docopt
-} = require('docopt')
-var fs = require('fs')
-var Promise = require('bluebird')
-var Logdown = require('logdown')
+    docopt
+} = require('docopt');
+var fs = require('fs');
+var Promise = require('bluebird');
+var Logdown = require('logdown');
 var logger = new Logdown({
-  prefix: 'wordspread'
-})
-var path = require('path')
+    prefix: 'wordspread'
+});
+var path = require('path');
 
 var form = require('form-urlencoded');
+var uri= require('strict-uri-encode');
 
 var {
-  link, msg
-} = require('./lib/print')
+    link,
+    msg
+} = require('./lib/print');
 
 
 var getOptions = doc => {
-  "use strict"
-  var o = docopt(doc)
-  var subreddit = o['--subreddit'] || 'javascript'
-  var title = o.TITLE
-  var url = o.LINK
-  var noshow = o['--no-show-hn'] || false
-  return {
-    subreddit, title, url, noshow
-  }
+    "use strict"
+    var o = docopt(doc);
+    var subreddit = o['--subreddit'] || 'javascript';
+    var title = o.TITLE;
+    var url = o.LINK;
+    var noshow = o['--no-show-hn'] || false;
+    return {
+        subreddit,
+        title,
+        url,
+        noshow
+    };
 }
 
-var doc = fs.readFileSync(__dirname + "/docs/usage.md", 'utf8')
+var doc = fs.readFileSync(__dirname + "/docs/usage.md", 'utf8');
 
 // https: //news.ycombinator.com/submitlink?u=https%3A%2F%2Fgithub.com%2Fvzaccaria%2Fdom-restyle&t=vzaccaria%2Fdom-restyle
 
 function postReddit(title, url, opts) {
-  var submiturl = "http://www.reddit.com/submit"
+    var submiturl = "http://www.reddit.com/submit";
 
-  var data = {
-      url: url,
-      title: title,
-      sr: opts.subreddit
-    }
+    var data = {
+        url: url,
+        title: title,
+        sr: opts.subreddit
+    };
     // body...
-  return `${submiturl}?${form.encode(data)}`
+    return `${submiturl}?${form.encode(data)}`;
 }
 
 function postHn(title, url, opts) {
-  "use strict"
-  var htitle
-  if (!opts.noshow) {
-    htitle = `Show HN: ${title}`
-  } else {
-    htitle = title
-  }
-  url = url
+    "use strict";
+    var htitle;
+    if (!opts.noshow) {
+        htitle = `Show HN: ${title}`;
+    } else {
+        htitle = title;
+    }
+    url = url;
 
-  var data = {
-    u: url,
-    t: htitle
-  }
-  return `https://news.ycombinator.com/submitlink?${form.encode(data)}`
+    var data = {
+        u: url,
+        t: htitle
+    };
+    return `https://news.ycombinator.com/submitlink?u=${uri(url)}&t=${uri(htitle)}`;
 }
 
 function postEcho(title, url) {
-  "use strict"
+    "use strict";
 
-  var data = {
-    u: url,
-    t: title
-  }
-  return `http://www.echojs.com/submit?${form.encode(data)}`
+    var data = {
+        u: url,
+        t: title
+    };
+    return `http://www.echojs.com/submit?${form.encode(data)}`;
 }
 
 function postTwitter(title, url) {
-  "use strict"
+    "use strict";
 
-  var data = {
-    url: url,
-    text: `${title} — `
-  }
-  return `http://twitter.com/share?${form.encode(data)}`
+    var data = {
+        url: url,
+        text: `${title} — `
+    };
+    return `http://twitter.com/share?${form.encode(data)}`;
 }
 
 // javascript:(function(){f='http://twitter.com/share?url='+encodeURIComponent(window.location.href)+'&text='+encodeURIComponent(document.title);a=function(){if(!window.open(f))location.href=f};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})()
 var main = () => {
-  "use strict"
-  var opts = (getOptions(doc))
-  var {
-    title, url
-  } = opts
+    "use strict";
+    var opts = (getOptions(doc));
+    var {
+        title,
+        url
+    } = opts;
 
-  msg()
-  logger.log("**HN**: ", link(postHn(title, url, opts)))
-  logger.log(`**Reddit /r/${opts.subreddit}**: `, link(postReddit(title, url, opts)))
-  logger.log(`**Echo JS**: `, link(postEcho(title, url)))
-  logger.log(`**Twitter**: `, link(postTwitter(title, url)))
+    msg();
+    logger.log("**HN**: ", link(postHn(title, url, opts)));
+    logger.log(`**Reddit /r/${opts.subreddit}**: `, link(postReddit(title, url, opts)));
+    logger.log(`**Echo JS**: `, link(postEcho(title, url)));
+    logger.log(`**Twitter**: `, link(postTwitter(title, url)));
 }
 
-main()
+main();
